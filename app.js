70713f5283390d3eba4169c18e6b6d4f9d58cca0
Hùng Lê Minh <haless132@gmail.com>
2022-10-16 11:47:38 +0700

    fix

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.use((req, res, next) => {
   next();
 });
 
-app.use("/api/posts", postRouters);
+app.use("/posts", postRouters);
 
 app.use((req, res, next) => {
   const error = new Error("Not found");
