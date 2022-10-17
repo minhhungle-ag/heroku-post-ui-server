f11e28cfb0a1156d171670969589ba7fecfdd969
minhhungle-ag <haless132@gmail.com>
2022-10-17 22:31:58 +0700

    fix

--- api/routes/upload.js
+++ api/routes/upload.js
@@ -27,10 +27,10 @@ function fileFilter(req, file, cb) {
 
 const upload = multer({
     storage: storage,
-    limits: {
-        fieldSize: 1024 * 607 * 5,
-    },
-    fileFilter: fileFilter,
+    // limits: {
+    //     fieldSize: 1024 * 607 * 5,
+    // },
+    // fileFilter: fileFilter,
 })
 
 router.post('/', upload.single('imageUrl'), (req, res, next) => {
